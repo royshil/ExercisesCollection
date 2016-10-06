8747e3aaeda99c5984e3499f54c72fc3f5c4761b
Roy Shilkrot <roy.shil@gmail.com>
2016-10-06 16:02:11 -0400

    main, makefile fix

--- ActiveContours/main.cpp
+++ ActiveContours/main.cpp
@@ -5,6 +5,18 @@
  *      Author: roy_shilkrot
  */
 
+#include "IntelligentScissors.h"
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv) {
+	Mat image = imread("input.jpg");
+
+	IntelligentScissors segmenter(image);
+	segmenter.segment();
+}
 
